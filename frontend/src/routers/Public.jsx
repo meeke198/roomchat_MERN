7ae91242f2fbbf9741560cd89fb5542d78edb88b
hien nguyen <thuhiennguyen198@gmail.com>
2022-03-01 18:22:23 -0500

    remove pupblic routes

--- frontend/src/routers/Public.jsx
+++
@@ -1,9 +0,0 @@
-import { Route, Redirect, withRouter } from "react-router-dom";
-
-const PublicRoute = ({ component: Component, ...rest }) => {
-//   const isLoggedIn = localStorage.getItem("access_token");
-
-  return <Route {...rest} render={(props) => <Component {...props} />} />;
-};
-
-export default PublicRoute;
