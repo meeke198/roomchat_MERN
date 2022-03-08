18e63e94226838482fc3623e4f564a135c0ccdc7
hien nguyen <thuhiennguyen198@gmail.com>
2022-03-07 22:47:20 -0500

    remove comments

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -196,7 +196,6 @@ const handleOnSubmit = async (e) => {
           <div>
             <button
               type="submit"
-              /*hover:bg-indigo-700 */
               className={`${
                 allowSubmit
                   ? "bg-indigo-700"
@@ -204,7 +203,6 @@ const handleOnSubmit = async (e) => {
               } group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                {/* Heroicon name: solid/lock-closed */}
                 <svg
                   className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
                   xmlns="http://www.w3.org/2000/svg"
