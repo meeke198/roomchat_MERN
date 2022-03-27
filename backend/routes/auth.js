3a79906742783db3ce19d260dfc20562cc5d7d9b
hien nguyen <thuhiennguyen198@gmail.com>
2022-03-27 12:17:14 -0400

    modified auth

---
+++ backend/routes/auth.js
@@ -0,0 +1,9 @@
+routes / auth.js;
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+  res.json("goi login");
+});
+
+module.exports = router;
