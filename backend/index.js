c5c753d08a245ae6f0438d412ac3d8152157ca0f
hien nguyen <thuhiennguyen198@gmail.com>
2022-06-13 01:29:44 -0400

    add socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+// const http = require('http')
+// const socketio = require('socket.io')
+// const server = http.createServer(app);
+
 require("dotenv").config(); //de doc duoc file env bang cach process.env.abc
 const bodyParser = require("body-parser");
 const auth = require("./routes/auth");
@@ -8,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/auth", auth); //
+// const io = socketio(server)
+// oninput.on('connection', socket => {
+//     console.log('New WS connection....')
+// })
 
 const port = process.env.PORT || 5000;
 
