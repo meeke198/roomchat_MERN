8dad1bd54f631fbc6d4a13dc35539b4ffc5e87a0
hien nguyen <thuhiennguyen198@gmail.com>
2022-07-22 00:24:02 -0400

    remove fetch

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,25 +5,5 @@ export default ({
     login: async ( email, password) => {
         return await Promise.resolve("Sucess")
         .then((data) => console.log(data))
-        // const result = await axios.post("")
-//         const result = await fetch("http://localhost5001/api/auth/signup", {
-//            method: "POST",
-//            headers: {
-//              "Content-Type": "application/json",
-//            },
-//            body: JSON.stringify({
-//              username,
-//              email,
-//              password,
-//            }),
-//          })
-//            .then((res) => res.json())
-//            .then((data) => {
-//             //  let newUser = data;
-//              console.log(data);
-//            })
-//            .catch((err) => {
-//              console.log(err);
-        //    });
     }
 })
